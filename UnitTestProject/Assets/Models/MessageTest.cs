c35cef8f0be68beb101500672d8ecde1982cab71
sdercolin <sdercolin@outlook.jp>
2018-07-08 22:50:32 +0900

    Create UnitTestProject

---
+++ UnitTestProject/Assets/Models/MessageTest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject
+{
+    [TestClass]
+    public class MessageTest
+    {
+        [TestMethod]
+        public void CloneTest()
+        {
+            EmptyMessage message1 = new EmptyMessage();
+            Message clone1 = message1.Clone();
+            Assert.IsTrue(clone1 is EmptyMessage);
+
+            var card = new Card00001(1, new Game().Player);
+            var targets = new List<Card>() { card };
+            MoveMessage message2 = new MoveMessage()
+            {
+                ReasonCard = card,
+                Targets = targets
+            };
+            Message clone2 = message2.Clone();
+            Assert.IsTrue(clone2 is MoveMessage);
+            Assert.IsTrue(clone2.ReasonCard == card);
+            Assert.IsFalse(clone2.Targets == targets);
+            Assert.IsTrue(clone2.Targets.SequenceEqual(targets));
+        }
+    }
+}
---
+++ UnitTestProject/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestProject")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestProject")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("b89533f8-92f1-4133-ab6e-9918e03f49a4")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
