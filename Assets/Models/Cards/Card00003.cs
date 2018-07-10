dde73e79465de83138d50fdf11d810198be83c0d
sdercolin <sdercolin@outlook.jp>
2018-07-11 03:46:02 +0900

    Create Card00003

---
+++ Assets/Models/Cards/Card00003.cs
@@ -0,0 +1,52 @@
+/// <summary>
+/// (S01) S01-003 忠義の古強者 ジェイガン
+/// </summary>
+public class Card00003 : Card
+{
+    public Card00003(int id, User controller) : base(id, controller)
+    {
+        Serial = "00003";
+        Pack = "S01";
+        CardNum = "S01-003";
+        Title = "忠义的老战士";
+        UnitName = "杰刚";
+        power = 70;
+        support = 20;
+        deployCost = 3;
+        classChangeCost = 2;
+        symbols.Add(SymbolEnum.Red);
+        genders.Add(GenderEnum.Male);
+        weapons.Add(WeaponEnum.Lance);
+        types.Add(TypeEnum.Beast);
+        ranges.Add(RangeEnum.One);
+        sk1 = new Sk1();
+        Attach(sk1);
+    }
+
+    /// <summary>
+    /// スキル1
+    /// 『戦場の教育役』【特】このカードは絆エリアに置くことができない。
+    /// </summary>
+    Sk1 sk1;
+    public class Sk1 : PermanentSkill
+    {
+        public Sk1()
+        {
+            Number = 1;
+            Name = "战场上的教育家";
+            Description = "『战场上的教育家』【特】这张卡不能放置到羁绊区。";
+            TypeSymbols.Add(SkillTypeSymbol.Special);
+            Keyword = SkillKeyword.Null;
+        }
+
+        public override bool CanTarget(Card card)
+        {
+            return card == Owner;
+        }
+
+        public override void SetItemToApply(Card target)
+        {
+            Owner.Attach(new CanNotBePlacedInBond());
+        }
+    }
+}
