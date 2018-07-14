522811834472ab730a6f3fadb00d0048d2271d78
sdercolin <sdercolin@outlook.jp>
2018-07-14 18:48:55 +0900

    Create JsonUtils

---
+++ Assets/Utils/JsonUtils.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Newtonsoft.Json;
+
+public static class JsonUtils
+{
+    public static string ToJson(object any)
+    {
+        return JsonConvert.SerializeObject(any);
+    }
+
+    public static object FromJson<T>(string json)
+    {
+        return JsonConvert.DeserializeObject<T>(json);
+    }
+}
