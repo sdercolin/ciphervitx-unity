9177406f914732dc450274dbc37df5936f2d1634
sdercolin <sdercolin@outlook.jp>
2018-07-14 18:49:30 +0900

    Refactor Request<T> to Request

--- Assets/Models/Request.cs
+++ Assets/Models/Request.cs
@@ -3,9 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-static class Request<T>
+public static class Request
 {
-    public static T ChooseUpToOne(List<T> choices)
+    public static T ChooseUpToOne<T>(List<T> choices)
     {
         List<T> results = Choose(choices, 0, 1);
         if (results.Count == 0)
@@ -18,32 +18,32 @@ static class Request<T>
         }
     }
 
-    public static T ChooseOne(List<T> choices)
+    public static T ChooseOne<T>(List<T> choices)
     {
         return Choose(choices, 1, 1)[0];
     }
 
-    public static List<T> ChooseUpTo(List<T> choices, int max)
+    public static List<T> ChooseUpTo<T>(List<T> choices, int max)
     {
         return Choose(choices, 0, max);
     }
 
-    public static List<T> Choose(List<T> choices, int number)
+    public static List<T> Choose<T>(List<T> choices, int number)
     {
         return Choose(choices, number, number);
     }
 
-    public static List<T> Choose(List<T> choices)
+    public static List<T> Choose<T>(List<T> choices)
     {
         return Choose(choices, 0, choices.Count);
     }
 
-    public static List<T> Choose(List<T> choices, int min, int max)
+    public static List<T> Choose<T>(List<T> choices, int min, int max)
     {
         throw new NotImplementedException();
     }
 
-    public static bool AskIfUse(T target)
+    public static bool AskIfUse<T>(T target)
     {
         throw new NotImplementedException();
     }
