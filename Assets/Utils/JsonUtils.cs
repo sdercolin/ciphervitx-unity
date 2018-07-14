e1ca6886447c5593d2ad5dc289bd41302bcfea97
sdercolin <sdercolin@outlook.jp>
2018-07-15 00:22:33 +0900

    Change settings of JsonUtils

--- Assets/Utils/JsonUtils.cs
+++ Assets/Utils/JsonUtils.cs
@@ -3,16 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 public static class JsonUtils
 {
+    public static JsonSerializerSettings settings = new JsonSerializerSettings();
+
+    static JsonUtils()
+    {
+        settings.ReferenceLoopHandling = ReferenceLoopHandling.Serialize;
+        settings.PreserveReferencesHandling = PreserveReferencesHandling.All;
+        settings.TypeNameHandling = TypeNameHandling.All;
+        settings.TypeNameAssemblyFormatHandling = TypeNameAssemblyFormatHandling.Simple;
+    }
+
     public static string ToJson(object any)
     {
-        return JsonConvert.SerializeObject(any);
+        return JsonConvert.SerializeObject(any, settings);
     }
 
     public static object FromJson<T>(string json)
     {
-        return JsonConvert.DeserializeObject<T>(json);
+        return JsonConvert.DeserializeObject<T>(json, settings);
     }
 }
