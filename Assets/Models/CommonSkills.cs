d4ffe4ec29f0918cc59a2cec245c1cc9d126e3d4
sdercolin <sdercolin@outlook.jp>
2018-08-23 20:06:17 +0900

    Add Wingslayer as a common skill

---
+++ Assets/Models/CommonSkills.cs
@@ -0,0 +1,17 @@
+﻿/// <summary>
+/// 『飛行特効』【常】このユニットが<飛行>を攻撃している場合、このユニットの戦闘力は＋３０される。
+/// </summary>
+public class Wingslayer : PermanentSkill
+{
+    public override bool CanTarget(Card card)
+    {
+        return card == Owner
+            && Game.AttackingUnit == card
+            && Game.DefendingUnit.HasType(TypeEnum.Flight);
+    }
+
+    public override void SetItemToApply()
+    {
+        ItemsToApply.Add(new PowerBuff(this, 30));
+    }
+}
