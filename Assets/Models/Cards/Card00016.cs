f060a9d188fcdd8dd009bb9a37a0db56231ba150
DarkKnight0_0 <xyz19940216@163.com>
2018-08-23 13:49:58 +0800

    Add Card00016

---
+++ Assets/Models/Cards/Card00016.cs
@@ -0,0 +1,74 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+/// <summary>
+/// (S01) B01-019 タリスの戦士 サジ
+/// </summary>
+public class Card00016 : Card
+{
+    public Card00016(User controller) : base(controller)
+    {
+        Serial = "00016";
+        Pack = "S01";
+        CardNum = "B01-019";
+        Title = "塔利斯的战士";
+        UnitName = "萨基";
+        power = 30;
+        support = 10;
+        deployCost = 1;
+        classChangeCost = 0;
+        symbols.Add(SymbolEnum.Red);
+        genders.Add(GenderEnum.Male);
+        weapons.Add(WeaponEnum.Axe);
+        ranges.Add(RangeEnum.One);
+        sk1 = new Sk1();
+        Attach(sk1);
+        sk2 = new Sk2();
+        Attach(sk2);
+    }
+
+    /// <summary>
+    /// スキル1
+    /// 『戦士の心得』【常】自分のターン中、このユニットの戦闘力は＋２０される。
+    /// </summary>
+    public Sk1 sk1;
+    public class Sk1 : PermanentSkill
+    {
+        public Sk1() : base()
+        {
+            Number = 1;
+            Name = "战士的心得";
+            Description = "『战士的心得』【常】自己的回合中，这名单位的战斗力+20。";
+            TypeSymbols.Add(SkillTypeSymbol.Permanent);
+            Keyword = SkillKeyword.Null;
+        }
+
+        public override bool CanTarget(Card card)
+        {
+            return card == Owner
+                && Game.TurnPlayer == Owner.Controller;
+        }
+
+        public override void SetItemToApply()
+        {
+            ItemsToApply.Add(new PowerBuff(this, 20));
+        }
+    }
+
+    /// <summary>
+    /// スキル2
+    /// 〖攻击型〗『攻撃の紋章』【支】戦闘終了まで、自分の攻撃ユニットの戦闘力は＋２０される。
+    /// </summary>
+    public Sk2 sk2;
+    public class Sk2 : AttackEmblem
+    {
+        public Sk2() : base()
+        {
+            Number = 2;
+            Name = "攻击之纹章";
+            Description = "〖攻击型〗『攻击之纹章』【支】直到战斗结束为止，我方的攻击单位的战斗力+20。";
+            TypeSymbols.Add(SkillTypeSymbol.Support);
+            Keyword = SkillKeyword.Null;
+        }
+    }
+}
