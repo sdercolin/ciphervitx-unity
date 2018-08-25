f02a2da0f34b64e619c1b808121a78a8f119d9e0
DarkKnight0_0 <xyz19940216@163.com>
2018-08-25 22:49:30 +0800

    Add Card00041

---
+++ Assets/Models/Cards/Card00041.cs
@@ -0,0 +1,76 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+/// <summary>
+/// (S02) B01-067 お人好しな碧騎士 ソール
+/// </summary>
+public class Card00041 : Card
+{
+    public Card00041(User controller) : base(controller)
+    {
+        Serial = "00041";
+        Pack = "S02";
+        CardNum = "B01-067";
+        Title = "忠厚的碧骑士";
+        UnitName = "索尔";
+        power = 40;
+        support = 10;
+        deployCost = 1;
+        classChangeCost = 0;
+        symbols.Add(SymbolEnum.Blue);
+        genders.Add(GenderEnum.Male);
+        weapons.Add(WeaponEnum.Sword);
+        types.Add(TypeEnum.Beast);
+        ranges.Add(RangeEnum.One);
+        sk1 = new Sk1();
+        Attach(sk1);
+        sk2 = new Sk2();
+        Attach(sk2);
+    }
+
+    /// <summary>
+    /// スキル1
+    /// 『碧と紅の絆』【常】このユニットが『ソワレ』に支援されている場合、このユニットの戦闘力は＋３０される。
+    /// </summary>
+    public Sk1 sk1;
+    public class Sk1 : PermanentSkill
+    {
+        public Sk1() : base()
+        {
+            Number = 1;
+            Name = "碧与红的羁绊";
+            Description = "『碧与红的羁绊』【常】这名单位被「索瓦蕾」支援的期间，这名单位的战斗力+30。";
+            TypeSymbols.Add(SkillTypeSymbol.Permanent);
+            Keyword = SkillKeyword.Null;
+        }
+
+        public override bool CanTarget(Card card)
+        {
+            return card == Owner
+                && Game.BattlingUnits.Contains(card)
+                && card.Controller.Support.Filter(supportCard => supportCard.HasUnitNameOf("索瓦蕾")).Count > 0;
+        }
+
+        public override void SetItemToApply()
+        {
+            ItemsToApply.Add(new PowerBuff(this, 30));
+        }
+    }
+
+    /// <summary>
+    /// スキル2
+    /// 〖攻击型〗『攻撃の紋章』【支】戦闘終了まで、自分の攻撃ユニットの戦闘力は＋２０される。
+    /// </summary>
+    public Sk2 sk2;
+    public class Sk2 : AttackEmblem
+    {
+        public Sk2() : base()
+        {
+            Number = 2;
+            Name = "攻击之纹章";
+            Description = "〖攻击型〗『攻击之纹章』【支】直到战斗结束为止，我方的攻击单位的战斗力+20。";
+            TypeSymbols.Add(SkillTypeSymbol.Support);
+            Keyword = SkillKeyword.Null;
+        }
+    }
+}
