b4a80569ef18e05f2bf2a963014e80dc0839a1dc
DarkKnight0_0 <xyz19940216@163.com>
2018-08-25 14:39:03 +0800

    Add Card00037

---
+++ Assets/Models/Cards/Card00037.cs
@@ -0,0 +1,99 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+/// <summary>
+/// (S02) B01-063 貴族的な弓使い ヴィオール
+/// </summary>
+public class Card00037 : Card
+{
+    public Card00037(User controller) : base(controller)
+    {
+        Serial = "00037";
+        Pack = "S02";
+        CardNum = "B01-063";
+        Title = "贵族弓箭手";
+        UnitName = "维奥尔";
+        power = 30;
+        support = 20;
+        deployCost = 1;
+        classChangeCost = 0;
+        symbols.Add(SymbolEnum.Blue);
+        genders.Add(GenderEnum.Male);
+        weapons.Add(WeaponEnum.Bow);
+        ranges.Add(RangeEnum.Two);
+        sk1 = new Sk1();
+        Attach(sk1);
+        sk2 = new Sk2();
+        Attach(sk2);
+        sk3 = new Sk3();
+        Attach(sk3);
+    }
+
+    /// <summary>
+    /// スキル1
+    /// 『鋼の弓』【起】〖1回合1次〗[翻面1]ターン終了まで、このユニットの戦闘力は＋１０される。
+    /// </summary>
+    public Sk1 sk1;
+    public class Sk1 : ActionSkill
+    {
+        public Sk1() : base()
+        {
+            Number = 1;
+            Name = "钢弓";
+            Description = "『钢弓』【起】〖1回合1次〗[翻面1]直到回合结束为止，这名单位的战斗力+10。";
+            OncePerTurn = true;
+            TypeSymbols.Add(SkillTypeSymbol.Action);
+            Keyword = SkillKeyword.Null;
+        }
+
+        public override bool CheckConditions()
+        {
+            return true;
+        }
+
+        public override Cost DefineCost()
+        {
+            return Cost.ReverseBond(this, 1);
+        }
+
+        public override Task Do()
+        {
+            Owner.Attach(new PowerBuff(this, 10, LastingTypeEnum.UntilTurnEnds));
+            return Task.CompletedTask;
+        }
+    }
+
+    /// <summary>
+    /// スキル2
+    /// 『飛行特効』【常】このユニットが<飛行>を攻撃している場合、このユニットの戦闘力は＋３０される。
+    /// </summary>
+    public Sk2 sk2;
+    public class Sk2 : Wingslayer
+    {
+        public Sk2() : base()
+        {
+            Number = 2;
+            Name = "飞行特效";
+            Description = "『飞行特效』【常】这名单位攻击<飞行>属性单位的期间，这名单位的战斗力+30。";
+            TypeSymbols.Add(SkillTypeSymbol.Permanent);
+            Keyword = SkillKeyword.Null;
+        }
+    }
+
+    /// <summary>
+    /// スキル3
+    /// 〖攻击型〗『攻撃の紋章』【支】戦闘終了まで、自分の攻撃ユニットの戦闘力は＋２０される。
+    /// </summary>
+    public Sk3 sk3;
+    public class Sk3 : AttackEmblem
+    {
+        public Sk3() : base()
+        {
+            Number = 3;
+            Name = "攻击之纹章";
+            Description = "〖攻击型〗『攻击之纹章』【支】直到战斗结束为止，我方的攻击单位的战斗力+20。";
+            TypeSymbols.Add(SkillTypeSymbol.Support);
+            Keyword = SkillKeyword.Null;
+        }
+    }
+}
