2d4a08d6c2ee3df8633ab496d10491b9c49689b9
Colin Weng <weng.yitong@linecorp.com>
2018-11-12 18:11:38 +0900

    Create IService

---
+++ Assets/IService.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Threading.Tasks;
+
+public interface IService
+{
+    Task<bool> Connect(string url);
+    Task<bool> Join(string roomId);
+    Task<bool> Send(Message message);
+    Task<T> Request<T>(Message message);
+}
