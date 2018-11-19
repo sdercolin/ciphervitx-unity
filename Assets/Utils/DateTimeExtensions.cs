fc32afba99061f03f494ac3db09968712c78b28c
Colin Weng <weng.yitong@linecorp.com>
2018-11-19 13:42:10 +0900

    Create DateTimeExtensions

---
+++ Assets/Utils/DateTimeExtensions.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+
+public static class DateTimeExtensions
+{
+    static uint ToTimeStamp(this DateTime dateTime){
+        var timespan2 = dateTime - new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+        return (uint)timespan2.TotalSeconds;
+    } 
+}
+
