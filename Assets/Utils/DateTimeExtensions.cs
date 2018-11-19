282765ee135feaa0f3404659300be1399f134aa4
Colin Weng <weng.yitong@linecorp.com>
2018-11-19 13:46:01 +0900

    Fix DateTimeExtensions

--- Assets/Utils/DateTimeExtensions.cs
+++ Assets/Utils/DateTimeExtensions.cs
@@ -3,7 +3,7 @@
 
 public static class DateTimeExtensions
 {
-    static uint ToTimeStamp(this DateTime dateTime){
+    public static uint ToTimeStamp(this DateTime dateTime){
         var timespan2 = dateTime - new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
         return (uint)timespan2.TotalSeconds;
     } 
